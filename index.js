a60dba4769a084aa4a9e35e2a75d6b53c4d78f11
CybertronicEvidence <adejuwonevidence181@gmail.com>
2022-09-22 21:45:11 +0100

    Final commit

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
+const orderRoute = require('./routes/order')
+
 
 dotenv.config();
 // app
@@ -20,6 +24,12 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/products', productRoute)
+app.use('/api/carts', cartRoute)
+app.use('/api/orders', orderRoute)
+
+
 
 
 app.listen(process.env.PORT || 5000, () => {
